331aa32e71d977393c67d91c8e9228d398241d4a
Elk360 <126981353+Elk360@users.noreply.github.com>
2024-05-01 08:50:59 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
-#include <iostream>
-
-using namespace std;
+#include "window.h"
+#include "input.h"
+#include "renderer.h"
 
 int main() {
-    // Variables to store the two numbers
-    int num1, num2;
-    
-    // Prompting the user to enter the first number
-    cout << "Enter the first number: ";
-    cin >> num1;
-    
-    // Prompting the user to enter the second number
-    cout << "Enter the second number: ";
-    cin >> num2;
-    
-    // Calculating the sum
-    int sum = num1 + num2;
-    
-    // Displaying the sum
-    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
-    
+    GLFWwindow* window = initializeWindowFromConfig("config.json");
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        render();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
